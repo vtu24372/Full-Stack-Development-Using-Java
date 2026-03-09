74085e69fb5cac1e318a565db4bc0956e51a69dc
vtu24372 <vtu24372@veltech.edu.in>
2026-03-09 11:43:53 +0530

    Delete Weekly-Tasks directory

--- Weekly-Tasks/Week-1/Tasks/OrderManagementSystem/src/main/java/com/oms/ordermanagement/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oms.ordermanagement.repository;
-
-import com.oms.ordermanagement.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- Weekly-Tasks/Week-1/spring.java_based_bean_config/spring.java_based_bean_config/src/main/java/com/vir/b30/spring/java_based_bean_config/Publish.java
+++
@@ -1,5 +0,0 @@
-package com.vir.b30.spring.java_based_bean_config;
-
-public interface Publish {
-    void show();
-}
